33f11362d64d1c9d227f6cc30cf49c07ab97470e
TunaWithSalmon <browsgaming5000@gmail.com>
2021-03-09 22:47:10 +0100

    Updated sitemap

--- asciitools/sitemaps.py
+++ asciitools/sitemaps.py
@@ -3,8 +3,10 @@ from django.shortcuts import reverse
 
 
 class StaticViewSitemap(Sitemap):
+    changefreq = "monthly"
+
     def items(self):
-        return ['home', 'image_to_ascii']
+        return ['image_to_ascii', 'text_to_banner']
 
     def location(self, item):
         return reverse(item)
