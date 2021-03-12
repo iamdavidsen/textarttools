64c719bebecc32292a02cc03de11f74fdf8e97d7
TunaWithSalmon <browsgaming5000@gmail.com>
2021-03-12 21:52:55 +0100

    Added analytics

--- textarttools/settings.py
+++ textarttools/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -28,8 +27,6 @@ DEBUG = os.environ.get('DEBUG', 'False') == "True"
 
 ALLOWED_HOSTS = ["asciigenerator.net", "127.0.0.1"]
 
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
-
 
     'accounts',
     'asciitools'
@@ -54,6 +50,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
+
 ]
 
 ROOT_URLCONF = 'textarttools.urls'
@@ -77,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'textarttools.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -87,7 +86,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -106,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
@@ -131,20 +128,25 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-
 if not DEBUG:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-    LINODE_BUCKET_ACCESS_KEY=os.environ.get('LINODE_BUCKET_ACCESS_KEY', '')
-    LINODE_BUCKET_SECRET_KEY=os.environ.get('LINODE_BUCKET_SECRET_KEY', '')
+    LINODE_BUCKET_ACCESS_KEY = os.environ.get('LINODE_BUCKET_ACCESS_KEY', '')
+    LINODE_BUCKET_SECRET_KEY = os.environ.get('LINODE_BUCKET_SECRET_KEY', '')
 
-    AWS_S3_ENDPOINT_URL="https://asciigenerator.eu-central-1.linodeobjects.com"
-    AWS_S3_REGION_NAME="eu-central-1"
-    AWS_S3_USE_SSL=True
-    AWS_STORAGE_BUCKET_NAME="asciigenerator"
-    AWS_ACCESS_KEY_ID=LINODE_BUCKET_ACCESS_KEY
-    AWS_SECRET_ACCESS_KEY=LINODE_BUCKET_SECRET_KEY
-
+    AWS_S3_ENDPOINT_URL = "https://asciigenerator.eu-central-1.linodeobjects.com"
+    AWS_S3_REGION_NAME = "eu-central-1"
+    AWS_S3_USE_SSL = True
+    AWS_STORAGE_BUCKET_NAME = "asciigenerator"
+    AWS_ACCESS_KEY_ID = LINODE_BUCKET_ACCESS_KEY
+    AWS_SECRET_ACCESS_KEY = LINODE_BUCKET_SECRET_KEY
 
 AUTH_USER_MODEL = 'accounts.Account'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
