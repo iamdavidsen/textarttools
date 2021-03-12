2d553a1ac3dada7d491afc664fb8f21705708287
TunaWithSalmon <browsgaming5000@gmail.com>
2021-03-12 17:03:22 +0100

    Added robots.txt

--- asciitools/urls.py
+++ asciitools/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from asciitools.views import image_to_ascii_view, banner_generator_view
 
 urlpatterns = [
     path('', image_to_ascii_view, name="image_to_ascii"),
     path('ascii-text-banner-generator', banner_generator_view, name="text_to_banner"),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
 ]
