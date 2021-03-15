90c190f0e2d6b30518a2a1972a661a18a1d9e8bb
TunaWithSalmon <browsgaming5000@gmail.com>
2021-03-15 01:10:29 +0100

    Fucked up sitemap

--- asciitools/sitemaps.py
+++ asciitools/sitemaps.py
@@ -6,7 +6,7 @@ class StaticViewSitemap(Sitemap):
     changefreq = "monthly"
 
     def items(self):
-        return ['home', 'image_to_ascii', 'text_to_banner']
+        return ['image_to_ascii', 'text_to_banner']
 
     def location(self, item):
         return reverse(item)
--- asciitools/urls.py
+++ asciitools/urls.py
@@ -4,8 +4,7 @@ from django.views.generic.base import TemplateView
 from asciitools.views import image_to_ascii_view, banner_generator_view, home_view
 
 urlpatterns = [
-    path('', home_view, name="home"),
-    path('image-to-ascii', image_to_ascii_view, name="image_to_ascii"),
+    path('', image_to_ascii_view, name="image_to_ascii"),
     path('ascii-banner', banner_generator_view, name="text_to_banner"),
 ]
 
